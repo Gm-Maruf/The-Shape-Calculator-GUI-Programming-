c9c3015972ccdd0a768963e4329cc20a1d4fb69a
Md. Golam Maula Maruf <37345164+Gm-Maruf@users.noreply.github.com>
2018-10-14 00:13:46 +0600

    Calculations are here

---
+++ Model/Calculations.java
@@ -0,0 +1,13 @@
+package Model;
+import java.util.ArrayList;
+
+public class Calculations extends ArrayList <String>{
+
+	public MathOperationsList(){
+		super();
+		add("*");
+	    add("/");
+	    add("+");
+	    add("-");   
+	}
+}
